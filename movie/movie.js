0bc5dee659ad265cba6614303a01cd9b9254ab52
hyungoo <hyungu0611@gmail.com>
2019-06-18 09:28:29 +0900

    gogo

--- movie/movie.js
+++ movie/movie.js
@@ -30,7 +30,7 @@ getInfo = async (code) => {
     return  res;
 }
 
-getMovie = async () => {
+getMovie = async function(){
     const movies = await fetch('http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=430156241533f1d058c603178cc3ca0e&targetDt='+makeDay());
     const movieList  = await movies.json();
     const res = await movieList.boxOfficeResult.dailyBoxOfficeList;
@@ -40,7 +40,7 @@ getMovie = async () => {
 
 
 
-getPoster = async (name) => {
+getPoster = async function(name) {
     const movieDb = await fetch('https://api.themoviedb.org/3/search/movie?api_key=e74869b4f7d0e8c609e11e474ee85790&query='+name);
     const db  = await movieDb.json();
     const res = await db.results[0].poster_path;
@@ -49,8 +49,8 @@ getPoster = async (name) => {
 
 function makeResult() {
     getMovie()
-    .then(movies => {
-        movies.map( async movie => {
+    .then(function(movies) {
+        movies.map(async function(movie) {
             const poster = await getPoster(movie.movieNm);
             const temp= { onemovie: {
                 "movietitle": movie.movieNm,
@@ -63,7 +63,7 @@ function makeResult() {
             
         })}
     )
-    .then(() => {
+    .then(function() {
         console.log(AllMovie);
     })
 }
@@ -83,3 +83,19 @@ makeResult();
 // https://api.themoviedb.org/3/search/movie?api_key=e74869b4f7d0e8c609e11e474ee85790&query=%EC%B2%9C%EB%A1%9C%EC%97%AD%EC%A0%95:+%EC%B2%9C%EA%B5%AD%EC%9D%84+%EC%B0%BE%EC%95%84%EC%84%9C
 
 // 서치 예시.
+
+
+let myFirstPromise = new Promise(function (resolve, reject) {  
+    // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
+    // In this example, we use setTimeout(...) to simulate async code. 
+    // In reality, you will probably be using something like XHR or an HTML5 API.
+    setTimeout(function(){
+      resolve("Success!"); // Yay! Everything went well!
+    }, 250);
+  });
+  
+  myFirstPromise.then((successMessage) => {
+    // successMessage is whatever we passed in the resolve(...) function above.
+    // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+    console.log("Yay! " + successMessage);
+  });
